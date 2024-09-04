0e2d684bf6d65f468addc795469ebb1d898d50b5
Rezzy Yuhand <rezzyyuhand92@student.ub.ac.id>
2024-09-04 20:57:34 +0700

    responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,28 +8,22 @@ import { FaLocationDot } from "react-icons/fa6";
 
 const Footer = ({ className }) => {
   return (
-    <div className={`flex flex-col items-center gap-2 px-28 pt-9 pb-5 ${className}`}>
-        <div className='flex flex-row justify-evenly gap-20'>
-            <div className='flex flex-col gap-4'>
+    <div className={`flex flex-col items-center gap-5 lg:gap-2 px-28 pt-9 pb-5 ${className}`}>
+      <div className='flex flex-col justify-start gap-5 lg:gap-3'>
+        <div className='flex flex-col lg:flex-row justify-evenly gap-5 lg:gap-20'>
+            <div className='flex flex-col gap-2 lg:gap-4'>
               <h3 className='font-semibold text-xl'>Bumi Perkemahan Bedengan</h3>
               <div className='flex flex-col gap-8'>
                 <p className='text-inactive-gray'>Bumi Perkemahan Bedengan<br/>merupakan sebuah destinasi yang<br/>menawarkan camping ground<br/>dengan keindahan hutan pinus.</p>
-                <div className='flex flex-row gap-4'>
-                  <FaInstagram className='text-secondary text-xl'/>
-                  <FaWhatsapp className='text-secondary text-xl'/>
-                  <AiOutlineYoutube className='text-secondary text-xl'/>
-                  <FaLinkedinIn className='text-secondary text-xl'/>
-                  <FaTwitter className='text-secondary text-xl'/>
-                </div>
               </div>
             </div>
-            <div className='flex flex-col gap-4 text-inactive-gray'>
+            <div className='flex flex-col gap-2 lg:gap-4 text-inactive-gray'>
               <p className='text-secondary font-bold'>Link</p>
               <Link className='transition-colors hover:text-secondary duration-300' to="/">Beranda</Link>
               <Link className='transition-colors hover:text-secondary duration-300' to="/reservasi">Reservasi</Link>
               <Link className='transition-colors hover:text-secondary duration-300' to="/syarat-dan-ketentuan">Syarat & Ketentuan</Link>
             </div>
-            <div className='flex flex-col gap-4 text-inactive-gray'>
+            <div className='flex flex-col gap-2 lg:gap-4 text-inactive-gray'>
               <p className='text-secondary font-bold'>Contact</p>
               <div className='flex flex-row gap-2 items-center'>
                 <PiPhoneFill className='text-secondary text-xl'/>
@@ -45,7 +39,17 @@ const Footer = ({ className }) => {
               </div>
             </div>
         </div>
-        <p className='text-inactive-gray'>&copy;2024 Bumi Perkemahan Bedengan All rights reserved.</p>
+        <div className='w-full'>
+          <div className='flex flex-row gap-3 lg:gap-4'>
+            <FaInstagram className='text-secondary text-xl'/>
+            <FaWhatsapp className='text-secondary text-xl'/>
+            <AiOutlineYoutube className='text-secondary text-xl'/>
+            <FaLinkedinIn className='text-secondary text-xl'/>
+            <FaTwitter className='text-secondary text-xl'/>
+          </div>
+        </div>
+      </div>
+      <p className='text-inactive-gray'>&copy;2024 Bumi Perkemahan Bedengan All rights reserved.</p>
     </div>
   )
 }
